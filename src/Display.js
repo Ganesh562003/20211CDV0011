053a7d86487dac04b3a33eba5e0d31c059f7d9da
Ganesh562003 <ganeshhegde289@gmail.com>
2024-05-03 14:51:03 +0530

    third

--- src/Display.js
+++ src/Display.js
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
+import {Link} from 'react-router-dom';
+
 function Display(){
     const [data,setData]=useState([])
     useEffect(()=>{
@@ -20,28 +22,31 @@ function Display(){
         <div>
       
 
-      <div >
+      <div className="justify-item-center" >
         <div >
-        <h1 >Fetched data is</h1>
+        <h1 className="justify-self-center bg-slate-900 text-slate-50">Fetched data is</h1>
         <ul>
           
             <div>
-              <table className="border-fixed border-amber-300 border border-slate-500 ..." >
+              <table className="table-auto w-1/2 border-collapse border-spacing-x-px border-amber-300 border  ..." >
                 <thead>
-                <tr><th className="border border-slate-600 ...">ProductNames</th>
-                <th className="border border-slate-600 ...">ProductPrice</th>
-                <th className="border border-slate-600 ...">Productrating</th>
-                <th className="border border-slate-600 ...">Productavilability</th>
+                <tr><th className="border border-amber-600 bg-slate-900 text-slate-50 ...">Names</th>
+                <th className="border border-amber-600 bg-slate-900 text-slate-50 ...">Price</th>
+                <th className="border border-amber-600 bg-slate-900 text-slate-50 ...">Rating</th>
+                <th className="border border-amber-600 bg-slate-900 text-slate-50 ...">Avilability</th>
+                <th className="border border-amber-600 bg-slate-900 text-slate-50 ...">Action</th>
                 </tr>
                 </thead>
                 <tbody>
                 {data.map((datas)=>(
                 <tr>
-                <td className="border border-slate-700 ..." key={datas.productName}> {datas.productName}</td>
-                  <td className="border border-slate-700 ..." key={datas.productName}>{datas.price}</td>
-                  <td className="border border-slate-700 ..." key={datas.productName}> {datas.rating}</td>
-                  <td className="border border-slate-700 ..." key={datas.productName}> {datas.availability}</td>
-                </tr>) )}
+                <td className="place-content-center border border-amber-700 bg-slate-900 text-slate-50 ..." key={datas.productName}> {datas.productName}</td>
+                  <td className="border border-amber-700 bg-slate-900 text-slate-50 ..." key={datas.productName}>{datas.price}</td>
+                  <td className="border border-amber-700 bg-slate-900 text-slate-50 ..." key={datas.productName}> {datas.rating}</td>
+                  <td className="border border-amber-700 bg-slate-900 text-slate-50 ..." key={datas.productName}> {datas.availability}</td>
+                  <td className="border border-amber-700 bg-slate-900 text-slate-50 ...">
+                  <Link to={'/read/${data.productName}'}>Read</Link>
+                  </td></tr>) )}
                 </tbody>
               </table>
 
--- src/Read.js
+++ src/Read.js
@@ -1,46 +1,27 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 function Read(){
-    const [data,setData]=useState([])
-    useEffect(()=>{
-        const fetchData=async()=>{
-            try{
-              const response=await axios.get("http://localhost:3006/user");
-
-              setData(response.data);
-    
-            }catch(error){
-              console.log('error fetching data',error);
-            }
-          };
-          fetchData();
-        
+  const [data,setData]=useState([])
+  const { productName }=useParams();
+  useEffect(()=>{
+      axios.get("http://localhost:3006/user"+productName)
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
     },[])
     return(
-        <div>
-      
-
-      <div >
-        <div >
-        <h1 >Fetched data is</h1>
-        <ul>
-          {data.map((datas)=>(
-            <div>
+      <div className="container mx-auto sm p-4 ">
+              
+                <ul>
+                <li className="container px-60 py-10 border-transparent my-20 rounded-xl shadow-lg shadow-black-4000/500 bg-white" key={data.productName}> {data.productName}<br/>{data.price}<br/>{data.rating}<br/>{data.discount}<br/>{data.availability}</li>
+                </ul>
+                
 
 
-            <li  key={datas.productName}>{datas.price} </li>
-            <li  key={datas.productName}>{datas.rating}</li>
-            <li  key={datas.productName}>{datas.availability}</li>
-            
-            </div>
            
-          ))}
-         
-        </ul>
-        </div>
-      </div>
-      </div>
-
+          
+            </div>
+        
     )
 }
 
